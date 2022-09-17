c5c2b5b2633471f7880396ac71f35542dc3d0e66
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-17 20:57:08 +0300

    Coordinate x,y

---
+++ Work2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] Coordinate(double b1, double k1, double b2, double k2)
+{
+    double[] result = new double[2];
+    result[0] = (b2 - b1) / (k1 - k2);
+    result[1] = (k1 * result[0] + b1);
+
+    return result;
+}
+
+Console.Write("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+double[] result = Coordinate(b1, k1, b2, k2);
+Console.WriteLine($"Координаты точки пересечения ({result[0]}), ({result[1]})");
