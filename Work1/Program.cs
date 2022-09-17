fe13574392eef53252db1c8446646986f36a70ea
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-17 20:16:07 +0300

    numbers > 0

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveCounts(int m)
+{
+    int count = 0;
+    for (int i = 1; i <= m; i++)
+    {
+        Console.Write($"Введите {i} число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел:");
+int m = int.Parse(Console.ReadLine());
+
+int counts = PositiveCounts(m);
+Console.Write($"Количество четных чисел: {counts} ");
+
